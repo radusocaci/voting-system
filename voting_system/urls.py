c5a6b0c3b7da7a5316da57ffcbb9cbb5f96305ec
Socaci Radu <radusocaci@gmail.com>
2019-12-30 12:29:02 +0200

    forgot password functionality + session expiration time

--- voting_system/urls.py
+++ voting_system/urls.py
@@ -29,7 +29,23 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='password-reset.html'),
+        name='password-reset'
+    ),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(
+        template_name='password-reset-done.html'),
+        name='password_reset_done'
+    ),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password-reset-confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password-reset-complete.html'),
+        name='password_reset_complete'
+    )
 ]
 
 if settings.DEBUG:
