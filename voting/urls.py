4df67dbf2a49c60a2fbd21d978561105f75575ac
Socaci Radu <radusocaci@gmail.com>
2020-01-05 14:41:05 +0200

    cleanup

--- voting/urls.py
+++ voting/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from voting import views
 
 urlpatterns = [
-    path("<int:pk>/", views.votedash, name='votedash'),
-    path("<int:candidate_id>/<int:voting_session_id>/", views.vote, name='vote'),
-    path('', views.dashboard, name='dashboard'),
-    path('<int:voting_session_id/>', views.results_dashboard, name='results_dashboard')
+    path("vote/<int:pk>/", views.votedash, name='votedash'),
+    path("vote/<int:candidate_id>/<int:voting_session_id>/", views.vote, name='vote'),
+    path('vote/', views.dashboard, name='dashboard'),
+    path('results/', views.results_dashboard, name='results_dashboard')
 ]
